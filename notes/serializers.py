55c7af8d2f17e667b455caee44bd52659b7296f5
dekarnldy <dekarnldy@gmail.com>
2025-05-19 20:44:24 +0700

    api auth backend

---
+++ notes/serializers.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import UserProfile, Note
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        read_only_fields = ('id',)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = UserProfile
+        fields = ('user', 'bio', 'location')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
+    
+    def create(self, validated_data):
+        # Assign the current user to the note
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
